6c136896533863729fec1925e5a2ad398349e1ee
Documenter.jl <documenter@juliadocs.github.io>
2022-05-31 20:06:22 +0000

    build based on 66a59e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WittVectors","category":"page"},{"location":"#WittVectors","page":"Home","title":"WittVectors","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WittVectors.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WittVectors]","category":"page"}]
+}
